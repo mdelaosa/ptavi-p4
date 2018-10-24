2c94b213dd324c177b836523229f82a8c0ba67d1
Maria De La Osa Martinez <mdelaosa@alpha27.aulas.gsyc.urjc.es>
2018-10-24 14:31:02 +0200

    Servidor guarda IP

--- server.py
+++ server.py
@@ -10,27 +10,34 @@ import sys
 PORT = int(sys.argv[1])  # Puerto en el que escuchamos
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    diccionario = {}
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         IPc = self.client_address[0]  # IP del Cliente
         PORTc = self.client_address[1]  # Puerto del Cliente
+        #print('IP:', IPc, 'PORT:', PORTc)
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'), IPc, PORTc)
+            if line:
+                if line.decode('utf-8') == 'REGISTER':
+                    print("El cliente nos manda ", line.decode('utf-8'))
+                    user = line.decode('utf-8')
+                    self.diccionario["IP"] = IPc  # Diccionario con la IP
+                    self.diccionario["USER"] = user  # Diccionario con el Usuario
+        print(self.diccionario)
 
 
 if __name__ == "__main__":
     # Listens at localhost ('') port which I want
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
