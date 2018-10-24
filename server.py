2a111e63a75bf40e217840a0d07f81f467494944
Maria De La Osa Martinez <mdelaosa@alpha27.aulas.gsyc.urjc.es>
2018-10-24 15:13:20 +0200

    Servidor con IP

--- server.py
+++ server.py
@@ -14,25 +14,23 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    diccionario = {}
-
+    dicc = {}
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-        IPc = self.client_address[0]  # IP del Cliente
-        PORTc = self.client_address[1]  # Puerto del Cliente
-        # print('IP:', IPc, 'PORT:', PORTc)
+        #IPc = self.client_address[0]  # IP del Cliente
+        #PORTc = self.client_address[1]  # Puerto del Cliente
+        #print('IP:', IPc, 'PORT:', PORTc)
         for line in self.rfile:
-            if line:
-                if line.decode('utf-8') == 'REGISTER':
-                    print("El cliente nos manda ", line.decode('utf-8'))
-                    user = line.decode('utf-8')
-                    self.diccionario["IP"] = IPc  # Diccionario con la IP
-                    self.diccionario["USER"] = user  # Dicc con el Usuario
-        print(self.diccionario)
+            if line.decode('utf-8') == 'REGISTER':
+                print("El cliente nos manda ", line.decode('utf-8'))
+                user = line.decode('utf-8')
+                self.dicc['IP'] = self.client_address[0]  # Dicc con la IP
+                #self.dicc["USER"] = user  # Dicc con el Usuario
+        print(self.dicc)
 
 
 if __name__ == "__main__":
