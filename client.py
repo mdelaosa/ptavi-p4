e410bec1cad7f41fc175ebcb6e579e00a01caef5
Maria De La Osa Martinez <mdelaosa@alpha27.aulas.gsyc.urjc.es>
2018-10-24 13:37:18 +0200

    Cliente

--- client.py
+++ client.py
@@ -10,13 +10,16 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]  # IP del Servidor
 PORT = int(sys.argv[2])  # Puerto en el que ejecuta
-LINE = " ".join(sys.argv[3:])  # Mensaje
+METHOD = str.upper(sys.argv[3])  # Método
+USER = sys.argv[4]  # Usuario
+
+REGISTERSIP = METHOD + ' sip:' + USER + ' SIP/2.0\r\n'  # Método + Usuario
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando:", REGISTERSIP)
+    my_socket.send(bytes(METHOD, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
