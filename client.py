20cd375d9882c052b6d625201057e9db67a39eee
Maria De La Osa Martinez <mdelaosa@alpha28.aulas.gsyc.urjc.es>
2018-10-25 10:28:18 +0200

    Cliente Ej6

--- client.py
+++ client.py
@@ -10,17 +10,20 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]  # IP del Servidor
 PORT = int(sys.argv[2])  # Puerto en el que ejecuta
-METHOD = str.upper(sys.argv[3])  # Método
-USER = sys.argv[4]  # Usuario
-
-REGISTERSIP = METHOD + ' sip:' + USER + ' SIP/2.0\r\n'  # Método + Usuario
+LINE = sys.argv[3:]  # Método y usuario
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", REGISTERSIP)
-    my_socket.send(bytes(METHOD, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    registersip = ' '.join(LINE)
+    if registersip != "":
+        if LINE[0] == 'register':
+            registersip = 'REGISTER sip:' + LINE[1] + ' SIP/2.0\r\n'  # Método + Usuario
+            print("Enviando:", registersip)
+        my_socket.send(bytes(registersip, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
+    else:
+        print('No hay mensaje')
 
 print("Socket terminado.")
