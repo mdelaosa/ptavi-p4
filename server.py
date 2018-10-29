3bf21a9b7a3c95006ad91d864425149088ae3bc0
Maria De La Osa Martinez <mdelaosa@alpha14.aulas.gsyc.urjc.es>
2018-10-29 17:14:08 +0100

    Servidor

--- server.py
+++ server.py
@@ -11,6 +11,19 @@ import json
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """Echo server class."""
 
+    def json2register(self):
+        try:
+            with open('registered.json', 'r') as archivo_json:
+                self.dicc = json.load(archivo_json)
+        except:
+            pass
+
+    def register2json(self):
+        """Create json file."""
+        with open('registered.json', 'w') as archivo_json:
+            json.dump(self.list, archivo_json, sort_keys=True,
+                      indent=4, separators=(',', ':'))
+
     def handle(self):
         """Handle method of the server class."""
         while 1:
@@ -22,37 +35,36 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 print("Petición recibida \r\n")
             if line_client[0] == 'REGISTER':
                 direction = line_client[1].split(':')
-                USER = direction[1]
-                IP = self.client_address[0]
-                EXPIRES = int(line_client[4])
+                user = direction[1]
+                ip = self.client_address[0]
+                expires = int(line_client[4])
                 time_actual = int(time.time())
                 time_actual_str = time.strftime('%Y-%m-%d %H:%M:%S',
                                                 time.gmtime(time_actual))
-                time_exp = int(EXPIRES + time_actual)
+                time_exp = int(expires + time_actual)
                 time_exp_str = time.strftime('%Y-%m-%d %H:%M:%S',
                                              time.gmtime(time_exp))
                 self.list = []
-                dicc[USER] = {'IP': IP, 'EXPIRES': time_exp_str}
-                self.lista.append(dicc)
+                dicc[user] = {'IP': ip, 'EXPIRES': time_exp_str}
+                self.list.append(dicc)
+
                 print('SIP/2.0 200 OK\r\n')
                 self.wfile.write(b"SIP/2.0 200 OK\r\n")  # Escritura socket
                 if line_client[4] == '0':
                     print('DELETING')
-                    del dicc[USER]
+                    del dicc[user]
                     print(self.list)
                 else:
                     print(self.list)
-
+                if time_actual_str >= time_exp_str:
+                    del dicc[user]
         self.register2json()
 
-    def register2json(self):
-        """Create json file."""
-        with open('registered.json', 'w') as archivo_json:
-            json.dump(self.list, archivo_json, sort_keys=True,
-                      indent=4, separators=(',', ':'))
 
     def removeclient(self):
         """Delete client once it has expired."""
+
+
 
 
 if __name__ == "__main__":
